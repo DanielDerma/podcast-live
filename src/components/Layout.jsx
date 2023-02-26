c8b1ca5a4e47d8040002c12c397a14370e81e135
danielderma <danielderma.contact@gmail.com>
2023-02-26 11:21:14 -0600

    Revert "feature: intial typescript setup"
    This reverts commit 9f844cc51f408ba0b1df6a45c08d393731c9c789.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
-import clsx from 'clsx'
+import { useId, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useId, useState } from 'react'
+import clsx from 'clsx'
 
 import { AudioPlayer } from '@/components/player/AudioPlayer'
 import posterImage from '@/images/poster.png'
